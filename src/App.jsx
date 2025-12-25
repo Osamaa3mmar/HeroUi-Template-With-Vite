2af04bd1cfafef3ad6a3463a7b4d3101b8081931
Osamaa3mmar <osamaammarothman@gmail.com>
2025-12-25 22:16:13 +0200

    Add: MainLayout component and update routing in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import { Route, Routes } from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
 
 function App() {
   return (
     <Routes>
-      <Route element={"test"} path="/" />
+      <Route element={<MainLayout />} path="/">
+        <Route element={"inside Layout"} path="inside" />
+        <Route element={"inside Layout"} path="" />
+      </Route>
     </Routes>
   );
 }
