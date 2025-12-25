0b4e88fea580b950ba7d540c3909d530e812d00d
Osamaa3mmar <osamaammarothman@gmail.com>
2025-12-25 22:11:46 +0200

    Remove: unused imports and clean up App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,9 @@
-import { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
-import { setTitle } from "./utils/SEOTools";
-
 
 function App() {
-
-  useEffect(()=>{
-    setTitle("Profile");
-  },[])
-
-
-
   return (
     <Routes>
       <Route element={"test"} path="/" />
-      
     </Routes>
   );
 }
